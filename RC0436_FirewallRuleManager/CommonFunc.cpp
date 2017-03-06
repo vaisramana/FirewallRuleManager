ee3ddc6b4b7ea0bc532a1a577a102f6672134e96
vaisramana <zhangshuo.chn@gmail.com>
2017-03-06 17:29:59 +0800

    move common definition to CommonFunc

--- RC0436_FirewallRuleManager/CommonFunc.cpp
+++ RC0436_FirewallRuleManager/CommonFunc.cpp
@@ -1,17 +1,12 @@
-#ifndef COMMON_FUNC_HPP_
-#define COMMON_FUNC_HPP_
-
 
 #include "CommonFunc.hpp"
-#include "ACLRuleManager.hpp"
 
 
-/*
 
-void printACLRuleList(std::vector<ACLRule>  ruleList)
+void printACLRuleList(vector<ACLRule>  ruleList)
 {
-    std::vector<ACLRule>::iterator iter;
-    std::vector<int>::iterator iter2;
+    vector<ACLRule>::iterator iter;
+    vector<int>::iterator iter2;
     cout<<"rule table:"<<endl;
     for(iter=ruleList.begin(); iter!=ruleList.end(); iter++)
     {
@@ -26,7 +21,7 @@ void printACLRuleList(std::vector<ACLRule>  ruleList)
 }
 
 
-void printVector(std::vector<int> v)
+void printVector(vector<int> v)
 {
     std::vector<int>::iterator iter;
     for(iter=v.begin(); iter!=v.end(); iter++)
@@ -36,9 +31,9 @@ void printVector(std::vector<int> v)
     cout<<endl;
 }
 
-void printVectorVector(std::vector<std::vector<int> > v)
+void printVectorVector(vector<vector<int> > v)
 {
-    std::vector<int>::iterator iter;
+    vector<int>::iterator iter;
     cout<<"index table:"<<endl;
     for(int i=0; i<3; i++)
     {
@@ -51,6 +46,6 @@ void printVectorVector(std::vector<std::vector<int> > v)
     }
     
 }
-*/
-#endif
+
+
 
--- RC0436_FirewallRuleManager/CommonFunc.hpp
+++ RC0436_FirewallRuleManager/CommonFunc.hpp
@@ -5,15 +5,108 @@
 #include <functional>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
+#include <boost/optional.hpp>
+#include <boost/asio.hpp>
+
+//#include "ACLRuleManager.hpp"
+
+
+
+using namespace std;
+using namespace boost;
+
+
+template <class T>
+using Optional = boost::optional<T>;
+
+template <class T>
+using In = const T&;
+
+template <class T>
+using Out = T&;
+
+template <class T> /**< Same C++ syntax as Out */
+using InOut = T&;
+
+
+
+using IndexList    = vector<int>;
+using IpAddress    = asio::ip::address;
+
+
+#define ACL_CHAIN_INDEX_RANGE (10000)
+#define ACL_CHAIN_INDEX_MIN (3000)
+#define ACL_CHAIN_INDEX_MARGIN (3000)
+#define ACL_OUTPUT_CHAIN_DSCP_INDEX_MIN (ACL_CHAIN_INDEX_MIN)
+#define ACL_OUTPUT_CHAIN_DSCP_INDEX_MAX (ACL_OUTPUT_CHAIN_DSCP_INDEX_MIN+ACL_CHAIN_INDEX_RANGE)
+#define ACL_OUTPUT_CHAIN_OTHER_INDEX_MIN (ACL_OUTPUT_CHAIN_DSCP_INDEX_MAX+ACL_CHAIN_INDEX_MARGIN)
+#define ACL_OUTPUT_CHAIN_OTHER_INDEX_MAX (ACL_OUTPUT_CHAIN_OTHER_INDEX_MIN+ACL_CHAIN_INDEX_RANGE)
+#define ACL_INPUT_CHAIN_INDEX_MIN (ACL_OUTPUT_CHAIN_OTHER_INDEX_MAX+ACL_CHAIN_INDEX_MARGIN)
+#define ACL_INPUT_CHAIN_INDEX_MAX (ACL_INPUT_CHAIN_INDEX_MIN+ACL_CHAIN_INDEX_RANGE)
+#define ACL_CHAIN_INDEX_MAX (ACL_INPUT_CHAIN_INDEX_MAX+ACL_CHAIN_INDEX_MARGIN)
+
+
+
+struct PortRange
+{
+    int lowValue;
+    int highValue;
+};
+
+using PortRanges = vector<PortRange>;
+
+
+typedef enum{
+   Input,
+   OutputDscp,
+   //OutputOthers,
+   /*new type should be added above this line*/
+   endOfAclChainType
+}ACLChainType;
+
+
+struct Rule
+{
+    int flowType;
+    optional<int> transportNetworkType;
+    optional<int> precedence;
+    optional<IpAddress> srcIp;
+    optional<int> srcPrefixLength;
+    optional<PortRanges> srcPortRanges;
+    optional<IpAddress> dstIp;
+    optional<int> dstPrefixLength;
+    optional<PortRanges> dstPortRanges;
+};
+
+
+struct ACLRule
+{
+    ACLChainType chainType;
+    Rule rule;
+    int dscp;
+    IndexList ruleIndexList; 
+};
+
+
+#if 0
+struct ACLRule
+{
+    ACLChainType chainType;
+    int rule;
+    int dscp;
+    vector<int> ruleIndexList; 
+};
+#endif
 
 
 
 
-/*
 void printACLRuleList(vector<ACLRule>  ruleList);
 void printVector(vector<int> v);
 void printVectorVector(vector<vector<int> > v);
-*/
+
 #endif
 
 
